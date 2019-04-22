b1f6c38734ce834527676729d21f65c27420fd09
Becka Henry <36617288+beckahenryl@users.noreply.github.com>
2019-04-21 17:44:22 -0700

    Update ShoppingCart.cpp

--- ObjectsZyLabShoppingCart/ShoppingCart.cpp
+++ ObjectsZyLabShoppingCart/ShoppingCart.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
-#include "ShoppingCart.h"
-#include "ItemToPurchase.cpp"
+#include <vector>
+#include <string>
+
 using namespace std;
 
-//calling original class ItemToPurchase
-ItemToPurchase item;
+#include "ShoppingCart.h"
+#include "ItemToPurchase.h"
+
+
 
 //default constructor
 //all private members are initialized to 0 or none
 ShoppingCart::ShoppingCart(){
 	customerName = "none";
-	currentDate = "January 1, 2016"
+	currentDate = "January 1, 2016";
 	vector <ItemToPurchase> cartItems;
 }
 
@@ -29,17 +32,71 @@ string ShoppingCart::GetDate() const{
 }
 
 //Adds an item to cartItems vector. Has parameter ItemToPurchase. Does not return anything.
-void ShoppingCart::AddItem(std::vector<ItemToPurchase> cartItems){
+/*
+	ADD ITEM TO CART
+	Enter the item name:
+	Nike Romaleos
+	Volt color, Weightlifting shoes
+	Enter the item price:
+	189
+	Enter the item quantity:
+	2
 
-	cartItems.push_back(item.GetName());
+*/
+void ShoppingCart::AddItem(ItemToPurchase object){
+	string nameItem, itemDescription;
+	int priceItem, quantityItem;
+
+
+	cout << "ADD ITEM TO CART" << endl;
+
+	cout << "Enter the item name:" << endl;
+	getline(cin, nameItem);
+	cout << endl;
+	cin.ignore();
+
+	cout << "Enter the item description:";
+	getline(cin, itemDescription);
+	cout << endl;
+
+	cout << "Enter the item price:" << endl;
+	cin >> priceItem;
+	cout << endl;
+
+	cout << "Enter the item quantity:" << endl;
+	cin >> quantityItem;
+	cout << endl;
+
+	//called the ItemToPurchase objects
+	object.SetName(nameItem);
+	object.SetDescription(itemDescription);
+	object.SetPrice(priceItem);
+	object.SetQuantity(quantityItem);
+
+	//add to vector
+	cartItems.push_back(object); 
 	
 }
 
 //Removes item from cartItems vector. Has a string (an item's name) parameter. Does not return anything.
 //If item name cannot be found, output this message: Item not found in cart. Nothing removed.
 void ShoppingCart:: RemoveItem(string name){
-	for (int i = 0; i < cartItems.size(); ++i){
-		if (cartItems.at(i) == name){
+	ItemToPurchase createObjectForOperator;
+	/*
+		REMOVE ITEM FROM CART
+		Enter name of item to remove:
+		Chocolate Chips
+
+	*/
+
+	cout << "REMOVE ITEM FROM CART" << endl;
+	cout << "Enter name of item to remove:" << endl;
+	getline(cin, name);
+	cout << endl;
+
+	for ( unsigned int i = 0; i < sizeof(cartItems); ++i){
+		createObjectForOperator = cartItems.at(i);
+		if (createObjectForOperator.GetName() == name){
 			cartItems.erase(cartItems.begin() + i);
 		}
 		else {
@@ -50,27 +107,67 @@ void ShoppingCart:: RemoveItem(string name){
 
 }
 
-//
-
 /*
 Modifies an item's description, price, and/or quantity. Has parameter ItemToPurchase. Does not return anything.
 If item can be found (by name) in cart, check if parameter has default values for description, price, and quantity. 
 If not, modify item in cart.
 If item cannot be found (by name) in cart, output this message: Item not found in cart. Nothing modified.
 */
-void ShoppingCart::ModifyItem(std::vector<ItemToPurchase> cartItems){
+void ShoppingCart::ModifyItem(ItemToPurchase object){
+	string nameItem;
+	int newQuantity;
+	ItemToPurchase createObjectForOperator;
 
+	cout << "CHANGE ITEM QUANTITY" << endl;
+	cout << "Enter the item name:" << endl;
+	getline(cin, nameItem);
+	cout << endl;
+	
+
+	for (unsigned int i = 0; i < sizeof(cartItems); ++i){
+		createObjectForOperator = cartItems.at(i);
+		if (createObjectForOperator.GetName() == nameItem){
+			//if the object does not have default values for description, price, and quantity, then
+			if (object.GetDescription() != "none" && object.GetQuantity() != 0 && object.GetPrice() != 0){
+				//modify the item in the cart
+				cout << "Enter the new quantity:" << endl;
+				cin >> newQuantity;
+				cout << endl;
+				object.SetQuantity(newQuantity);
+			}
+		}
+		//if item cannot be found by name in cart, then display message below
+		else{
+			cout << "Item not found in cart. Nothing modified" << endl;
+		}
+	}
 
 }
 
 //Returns quantity of all items in cart. Has no parameters.
 int ShoppingCart:: GetNumItemsInCart() const{
-	return item.GetQuantity();
+	int count = 0;
+
+	for (unsigned int i = 0; i < sizeof(cartItems); ++i){
+
+		count += 1; //add number of items in the cart
+	}
+
+	return count;
 }
 
+
 //Determines and returns the total cost of items in cart. Has no parameters.
-int ShoppingCart:: GetCostOfCart(){
-	item.GetPrice();
+int ShoppingCart:: GetCostOfCart() const{
+	ItemToPurchase accessMember;
+	int totalCost = 0;
+	for (unsigned int i = 0; sizeof(cartItems); ++i){
+		if (accessMember.GetPrice() >= 0){
+			totalCost += accessMember.GetPrice() * accessMember.GetQuantity();
+		}
+	}
+	
+	return totalCost;
 }
 
 /*
@@ -90,7 +187,21 @@ Total: $521
 
 */
 
-ShoppingCart::PrintTotal(){
+void ShoppingCart::PrintTotal(){
+
+	ShoppingCart classobjects;
+	ItemToPurchase object;
+	cout << classobjects.GetCustomerName() << "'s Shopping Cart - " << classobjects.GetDate() << endl;
+	cout << "Number of Items: " << classobjects.GetNumItemsInCart() << endl;
+	cout << endl;
+
+	for (unsigned int i = 0; i < sizeof(cartItems); ++i){
+		cout << object.GetName() << " " <<  object.GetQuantity() << "@ $" << object.GetPrice() << " = $" << object.GetPrice() * object.GetQuantity() << endl;
+	}
+	cout << endl;
+	//total cost of all items in cart taking into consideration the quantity
+	cout << "Total: $" << classobjects.GetCostOfCart() << endl;
+
 
 }
 
@@ -109,6 +220,16 @@ Powerbeats 2 Headphones: Bluetooth headphones
 
 */
 
-ShoppingCart::PrintDescriptions(){
+void ShoppingCart::PrintDescriptions(){
+	ShoppingCart classobjects;
+	ItemToPurchase object;
+
+	cout << classobjects.GetCustomerName() << "'s Shopping Cart - " << classobjects.GetDate() << endl;
+	cout << endl;
+
+	cout << "Item Descriptions" << endl;
+	for (unsigned int i = 0; sizeof(cartItems); ++i){
+		cout << object.GetName() << object.GetDescription() << endl; 
+	}
 
 }
